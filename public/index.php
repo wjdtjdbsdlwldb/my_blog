b84205760f9925a6eb2dbec6f5b20ce548b4c081
wjdtjdbsdlwldb <jms2464@naver.com>
2020-06-30 01:10:33 +0900

    프로필 추가, 수정 망함 2020-06-29

--- public/index.php
+++ public/index.php
@@ -4,22 +4,10 @@ include "../part/head.php";
 
 <div class="slider">
     <div class="slides">
-        <div style="background-image:url(/resource/img/bg_4.jpg);" class="bn-1 active">
-            <spen class="contents">JUNG SEO YUN</spen>
-            <spen class="contents contents1">WEB PUBLISHER & DESIGNER</spen>
-        </div>
-        <!-- <div style="background-image:url(/resource/img/bg_4.jpg);" class=""></div>
-        <div style="background-image:url(/resource/img/bg_4.jpg);" class=""></div> -->
-    </div>
+        <span class="contents con block">JUNG SEO YUN</span>
+        <span class="contents contents1 block con">WEB PUBLISHER & DESIGNER</span>
+        <div style="background-image:url(/resource/img/bg_4.jpg);" class="bn-1">
 
-    <div class="side-bars con">
-        <div>
-            <span>
-                <i class="fa fa-angle-left" aria-hidden="true"></i>
-            </span>
-        </div>
-        <div>
-            <span><i class="fa fa-angle-right" aria-hidden="true"></i></span>
         </div>
     </div>
 </div>
@@ -36,31 +24,29 @@ include "../part/head.php";
         <span class="profile">PROFILE</span>
         <div class="avatar"><img src="/resource/img/avatar.png" alt=""></div>
         <div class="line"><img src="/resource/img/line.png" alt=""></div>
-        <div class="infor"></div>
+        <div class="infor">
+            <div class="personal">
+                NAME&emsp;&emsp; 정서윤 <br><br>
+                BIRTH&emsp;&emsp; 1990.06.10 <br><br>
+                EMAIL&emsp;&emsp; jms2464@naver.com <br><br>
+                ADDRESS&emsp; &emsp;대전<br><br>
+                BLOG&emsp;&emsp; https://seoyun11.tistory.com/
+            </div>
+        </div>
     </div>
 </div>
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur natus nisi commodi animi, id blanditiis molestiae
-quia, ipsum veritatis quam esse. Ducimus aspernatur animi, ratione molestias vitae deserunt consectetur suscipit.
-Rerum praesentium natus pariatur vel eos aliquid adipisci blanditiis, voluptatem autem, ducimus nesciunt a illo veniam?
-Beatae possimus, nulla dolores corporis, quo commodi accusamus ullam distinctio atque aliquam, sapiente eos!
-Eius, nihil eaque ipsa non veritatis, aut accusantium nam nulla ea nesciunt ex. Voluptatem tempora ullam possimus animi.
-Suscipit recusandae architecto repudiandae necessitatibus animi quibusdam, quo quae autem asperiores laudantium?
-A, officia voluptates! Sequi placeat ipsum magni! Beatae, porro id? Commodi dolore, cumque modi asperiores fuga sunt
-minus enim itaque nisi earum nesciunt ad ab, magni saepe, at reprehenderit tempore.
-Explicabo quia sed totam, distinctio rerum dolor fugiat vitae quasi, neque tempora laborum culpa repellat? Et non
-distinctio quae. Sint, nihil possimus assumenda corrupti velit itaque consequatur iure veniam soluta?
-Asperiores aliquid aliquam voluptas facere necessitatibus, sed quisquam labore tempore incidunt nulla iusto quaerat
-officiis odio quod provident placeat debitis et. Ratione blanditiis expedita facere, hic neque reprehenderit voluptate
-corporis?
-Voluptas libero officiis doloribus numquam aliquid. Fuga pariatur error reprehenderit amet! Minus aspernatur illum odit
-quam nulla enim nobis, eveniet ipsum ex amet architecto eligendi incidunt debitis possimus libero accusantium?
-Blanditiis, similique! Nobis distinctio expedita blanditiis vitae ut error officiis obcaecati sit earum eos eligendi
-quia doloremque iste rem quam atque quas suscipit voluptatibus, odit esse, unde enim quibusdam? Itaque!
-Minus ex aspernatur doloremque. Ut odio fuga molestias, maxime asperiores non! Commodi fugiat totam molestias nemo
-animi! Voluptatibus iste maxime dolore aut consequatur non voluptatum facilis commodi ut praesentium. Excepturi.
-Cupiditate dignissimos quo ab libero, explicabo quod. Fugiat iste dolor obcaecati excepturi repellendus delectus unde
-rerum dolorum voluptates facere? Asperiores veniam a consequuntur quam corrupti ullam itaque illum quibusdam totam.
+
+<!-- my work 박스 시작 -->
+
+<div class="mywork">
+    <div class="my-text">MY WORK</div>
+    <img src="/resource/img/sub-icon2.png" alt="">
+</div>
+
+<div class="web-box con">
+    <div class="web-text">WEB CODING</div>
+</div>
 
 
 <?php
--- public/resource/common.js
+++ public/resource/common.js
@@ -36,42 +36,6 @@ function MobileSideBar__init() {
 
 $(function () {
     MobileSideBar__init();
-
-    $('.btn-toggle-mobile-side-bar').click();
-    $('.mobile-side-bar .menu-box-1 > ul > li:last-child').click();
-
-
-    $('.slider > .side-bars > div').click(function () {
-        var $clickedBtn = $(this);
-        var $slider = $clickedBtn.closest('.slider');
-
-        var isLeft = $clickedBtn.index() == 0;
-
-        var $currnet = $slider.find('.slides > .active');
-        var $post = null;
-
-        if (isLeft) {
-            $post = $currnet.prev();
-        } else {
-            $post = $currnet.next();
-        }
-
-        if ($post.length == 0) {
-            if (isLeft) {
-                $post = $slider.find('.slides > div:last-child');
-            } else {
-                $post = $slider.find('.slides > div:first-child');
-            }
-        }
-
-        $currnet.removeClass('active');
-        $post.addClass('active');
-    });
-   
 });
 
-function a () {
-    $('.slider > .side-bars > :last-child').click();
-  }
-  
-  setInterval (a,5000);
+
