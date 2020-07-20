90164561c8312a05eda916b9a13f40cd9de70ae8
wjdtjdbsdlwldb <jms2464@naver.com>
2020-07-20 18:33:47 +0900

    오늘도 한건 없지만 늘저장

---
+++ public/pf/md/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>(주)마켓디자이너스</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="./index.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ public/pf/sunny/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SUNNYISLAND</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+    <div class="top-bar">
+        <div class="main-top-box">
+            <div class="logo">
+                <a href="#"><img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/sunny/logo.png" alt=""></a>
+            </div>
+
+            <nav class="menu-box">
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+
+            <div class="icon-box">
+                <a href="#"><img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/sunny/ico_blog.png" alt=""></a>
+                <a href="#"><img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/sunny/ico_fb.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+</html>
