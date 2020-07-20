d04d3efc83dc19b40a1ba13385b51728b8cd72de
wjdtjdbsdlwldb <jms2464@naver.com>
2020-07-21 03:22:30 +0900

    엉망진창

--- public/pf/md/index.php
+++ public/pf/md/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +15,88 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <script src="./index.js"></script>
 </head>
+
 <body>
-    
+    <div class="layout">
+        <div class="main-top">
+            <div class="top-bar flex flex-ai-c">
+                <div class="logo flex-grow-1 con">
+                    <a href="#">
+                        <img src="
+                    https://wjdtjdbsdlwldb.github.io/img1/blog/article/md/logo.png" alt="">
+                    </a>
+                </div>
+                <nav class="menu-box flex">
+                    <ul class="flex">
+                        <li><a href="#">홈</a></li>
+                        <li><a href="#">마켓디자이너스</a></li>
+                        <li><a href="#">서비스</a></li>
+                        <li><a href="#">경영방침</a></li>
+                        <li><a href="#">인재채용 <span class="icon icon-nav"></span></a>
+                            <ul>
+                                <li><a href="#">진행 중 공고</a></li>
+                                <li><a href="#">엠디스토리</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">사회공헌 <span class="icon icon-nav"></span></a>
+                            <ul>
+                                <li><a href="#">해피이사 캠페인</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">뉴스룸</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="vd-box">
+            <div class="main-bg">
+                <video data-v-6e551807="" autoplay="autoplay" loop="loop" muted="muted" class="main__bg__video"
+                    draggable="true">
+                    <source data-v-6e551807="" src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/md/md_bg.mp4"
+                        type="video/mp4"></video>
+            </div>
+
+            <div class="content">
+                <div class="linkwrap">
+                    <ul class="linklist">
+                        <li class="item">
+                            <a href="#">
+                                <em>
+                                    Who
+                                    <span>
+                                        We are
+                                    </span>
+                                </em>
+                                <span>마켓디자이너스는 누구일까요?</span>
+                            </a></li>
+                        <li class="item">
+                            <a href="#">
+                                <em>
+                                    Our
+                                    <span>
+                                        Service
+                                    </span>
+                                </em>
+                                <span>우리 서비스를 소개합니다</span>
+                            </a></li>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <em>
+                                    Recruit
+                                    <span>
+                                        Now
+                                    </span>
+                                </em>
+                                <span>함께 할 분들을 기다립니다</span>
+                            </a></li>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
