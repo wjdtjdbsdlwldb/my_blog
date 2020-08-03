06884013cb2301f47bfb93ef826bf8ebdb428b1e
wjdtjdbsdlwldb <jms2464@naver.com>
2020-08-04 01:05:24 +0900

    메뉴바까지

--- public/pf/si/index.js
+++ public/pf/si/index.js
@@ -6,6 +6,41 @@ function FullPage() {
     });
 }
 
+function HoverEffect__init() {
+    var $Hover = $('.main > .header > .top-bar >.lan-box > .lan');
+
+    $Hover.mouseenter(function(){
+        $Hover.addClass('active');
+    });
+
+    $Hover.mouseleave(function(){
+        $Hover.removeClass('active');
+    });
+}
+
+
+function ListClick() {
+    $(".main>.header> .menu-box > ul > li").click(function (e) {
+        var $this = $(this);
+        var hasClass = $this.hasClass("active");
+
+        if (hasClass) {
+            $this.removeClass("active");
+        } else {
+            $this.siblings(".active").find("li.active").removeClass("active");
+            $this.siblings(".active").removeClass("active");
+            $this.addClass("active");
+        }
+    });
+    e.stopPropagation();
+}
+
+
 $(function(){
     FullPage();
+});
+
+$(function(){
+    HoverEffect__init();
+    ListClick();
 });
--- public/pf/si/index.php
+++ public/pf/si/index.php
@@ -30,16 +30,67 @@
                         <ul class="lan-box flex flex-jc-end">
                             <li class="si"><a href="#">S.I.VILLAGE </a></li>
                             <li class="lan"><a href="#">Korean</a>
-                            <ul>
-                                <li><a href="#">KOREAN</a></li>
-                                <li><a href="#">ENGLISH</a></li>
-                                <li><a href="#">CHINESE</a></li>
-                            </ul>
-                        </li>
+                                <ul>
+                                    <li><a href="#">KOREAN</a></li>
+                                    <li><a href="#">ENGLISH</a></li>
+                                    <li><a href="#">CHINESE</a></li>
+                                </ul>
+                            </li>
                         </ul>
-
                     </div>
+                    <nav class="menu-box">
+                        <ul class="side-mu">
+                            <li><a href="#">COMPANY</a>
+                                <ul>
+                                    <li><a href="#">회사소개</a></li>
+                                    <li><a href="#">비전</a></li>
+                                    <li><a href="#">연혁</a></li>
+                                    <li><a href="#">위치</a></li>
+                                    <li><a href="#">공고</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">BRAND</a>
+                                <ul>
+                                    <li><a href="#">패션</a></li>
+                                    <li><a href="#">뷰티</a></li>
+                                    <li><a href="#">리빙</a></li>
+                                </ul>
+
+                            </li>
+                            <li><a href="#">MEDIA</a></li>
+                            <li><a href="#">CAREER</a>
+                                <ul>
+                                    <li><a href="#">인재상</a></li>
+                                    <li><a href="#">지원하기</a></li>
+                                </ul>
+
+                            </li>
+                            <li><a href="#">INVESTORS</a>
+                                <ul>
+                                    <li><a href="#">기업지배구조</a></li>
+                                    <li><a href="#">주식/투자정보</a></li>
+                                    <li><a href="#">재무정보</a></li>
+                                    <li><a href="#">전자 공고</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">CSR</a>
+                                <ul>
+                                    <li><a href="#">CSR경영</a></li>
+                                    <li><a href="#">투명경영</a></li>
+                                    <li><a href="#">상생경영</a></li>
+                                    <li><a href="#">친환경경영</a></li>
+                                    <li><a href="#">사회공헌활동</a></li>
+                                    <li><a href="#">상담 및 제보</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">CONTACT</a></li>
+                        </ul>
+                    </nav>
                 </div>
+                <div class="main-bn">
+                    
+                </div>
+
             </div>
         </div>
         <div class="section">페이지 3</div>
