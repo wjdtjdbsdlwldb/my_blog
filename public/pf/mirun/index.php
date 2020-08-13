6196ab310e958418e70aa26447eaadb52d932b0d
wjdtjdbsdlwldb <jms2464@naver.com>
2020-08-13 18:46:54 +0900

    한게뭐냐 대체

--- public/pf/mirun/index.php
+++ public/pf/mirun/index.php
@@ -8,6 +8,11 @@
     <title>2020 MIRUN SEOUL</title>
     <link rel="stylesheet" href="./index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Slick 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
     <script src="./index.js"></script>
 
 </head>
@@ -16,8 +21,9 @@
     <div class="main con-min-width">
         <div class="header con">
             <div class="top-bar flex">
-                <div class="logo flex flex-grow-1">
-                    <a href="#"></a>
+                <div class="logo flex flex-grow-1 flex-ai-c">
+                    <a href="#"><img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/mirun/logo2.png"
+                            alt=""></a>
                 </div>
                 <nav class="menu-box flex flex-ai-c">
                     <ul class="flex">
@@ -30,12 +36,29 @@
             </div>
 
         </div>
-        <div class="bn-bar">
-            <div class="img-box">
-                <div></div>
+        <div class="main-bg">
+            <div class="slider-div">
+                <div class="bn1 bn">
+                    <p>
+                        TIME TO WAKE UP ENERGY</p>
+                </div>
+                <div class="bn2 bn">
+                    <p>
+                        TIME TO WAKE UP ENERGY</p>
+                </div>
+                <div class="bn3 bn">
+                    <p>
+                        TIME TO WAKE UP ENERGY</p>
+                </div>
             </div>
+            <div class="empty"></div>
+        </div>
+        
+        <div class="page-2">
+            <div class="white-bg"></div>
         </div>
     </div>
+
 </body>
 
 </html>
