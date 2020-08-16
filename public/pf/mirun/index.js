d3a6b50c3b7df7c48165ee67783f082000f7187c
wjdtjdbsdlwldb <jms2464@naver.com>
2020-08-16 19:37:51 +0900

    한게뭐람

--- public/pf/mirun/index.js
+++ public/pf/mirun/index.js
@@ -5,7 +5,7 @@ function Slider() {
         slidesToShow: 1, // 한 화면에 보여질 컨텐츠 개수
         slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
         speed: 1000, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-        arrows: true, // 옆으로 이동하는 화살표 표시 여부
+        arrows: false, // 옆으로 이동하는 화살표 표시 여부
         dots: false, //크롤바 아래 점으로 페이지네이션 여부
         autoplay: true, // 자동 스크롤 사용 여부
         autoplaySpeed: 3000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
--- public/pf/mirun/index.php
+++ public/pf/mirun/index.php
@@ -47,10 +47,6 @@
                         TIME TO WAKE UP ENERGY</p>
                 </div>
             </div>
-            
-            <div class="empty">
-                <div class="em-box"></div>
-            </div>
         </div>
 
         <div class="page-2">
@@ -64,16 +60,77 @@
                     <ul class="row">
                         <li class="cell">
                             <span class="block">
-                                <img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon1.jpg" alt="" srcset="">
-                                h3+p    
+                                <img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon1.png"
+                                    alt="" srcset="">
+                                <h3>INFORMATION</h3>
+                                <p>나의 첫 에너지 레이스가 되어 줄 마이런의<br>
+                                    참가 방법을 확인하세요.</p>
+                                <a href="#">
+                                    <p>참가안내</p>
+                                </a>
 
-                    </span></li>
-                        <li class="cell"><span class="block"><img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon4.jpg" alt="" srcset=""></span></li>
-                        <li class="cell"><span class="block"><img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon2.jpg" alt="" srcset=""></span></li>
-                        <li class="cell"><span class="block"><img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon3.jpg" alt="" srcset=""></span></li>
-                        <li class="cell"><span class="block"><img src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon5.jpg" alt="" srcset=""></span></li>
+                            </span></li>
+                        <li class="cell"><span class="block"><img
+                                    src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon2.png"
+                                    alt="" srcset="">
+                                <h3>REGISTRATION</h3>
+                                <p>참가신청은 선착순으로 마감될 수 있으니<br>
+                                    서둘러 마이런에 참가 신청하세요.</p>
+                                <a href="#">
+                                    <p>참가신청</p>
+                                </a>
+                            </span></li>
+                        <li class="cell"><span class="block"><img
+                                    src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon4.png"
+                                    alt="" srcset="">
+                                <h3>REGISTRATION INQUIRY</h3>
+                                <p>에너지 레이스 마이런에 참가 신청 한 후<br>
+                                    자신의 참가 내역을 조회해보세요.</p>
+                                <a href="#">
+                                    <p>참가조회</p>
+                                </a>
+                            </span></li>
+                        <li class="cell"><span class="block"><img
+                                    src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon3.png"
+                                    alt="" srcset="">
+                                <h3>INSURANCE JOIN</h3>
+                                <p>만일에 발생할 사고를 대비하여 미리 보험을<br>
+                                    가입하여 사고를 예방하세요.</p>
+                                <a href="#">
+                                    <p>보험가입</p>
+                                </a>
+                            </span></li>
+                        <li class="cell"><span class="block"><img
+                                    src="https://raw.githubusercontent.com/wjdtjdbsdlwldb/img1/master/blog/article/mirun/icon-box.png"
+                                    alt="" srcset="">
+                                <h3>RACE PACKAGE</h3>
+                                <p>마이런에서 기념으로 제공하는 패키지를<br>
+                                    확인하고 사이즈도 체크해보세요.</p>
+                                <a href="#">
+                                    <p>레이스 패키지</p>
+                                </a>
+                            </span></li>
+                        <li class="cell"><span class="block">
+                            </span></li>
                     </ul>
                 </nav>
+
+                <div class="woman">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/mirun/woman2.png" alt="">
+                    </div>
+                </div>
+                <div class="under">
+                    <div class="ud-whi"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="page-3">
+            <div class="grd-box">
+                <div class="infor"></div>
             </div>
         </div>
     </div>
