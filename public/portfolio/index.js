90e001276aa1146da386be57e925fb5e9a56c17a
wjdtjdbsdlwldb <jms2464@naver.com>
2020-08-30 20:12:27 +0900

    개뿔맞는지모를지경

--- public/portfolio/index.js
+++ public/portfolio/index.js
@@ -9,6 +9,64 @@ function FullPage() {
   });
 }
 
+function slider() {
+  $('.carousel-1 > .owl-carousel').owlCarousel({
+    navSpeed:150,
+    loop:false,
+    margin:10,
+    nav:false,
+    dots:false,
+    slideBy:4,
+    mouseDrag:true,
+  
+    responsive:{
+        0:{
+            items:5
+        }
+    }
+  });
+}
+ 
+function Slider1__itemClick(el) {
+  var $el = $(el);
+  var url = $el.attr('data-img-url');
+  var link = $el.attr('data-link-url');
+  var no = $el.attr('data-no');
+  var tx = $el.attr('data-tx');
+  var name = $el.attr('data-name');
+  var site = $el.attr('data-site');
+  
+  $('.img-box-1').empty().append("<a href='" + link + "' target='_blank'><img src='" + url + "'></a>");
+  $('.txt-box-1').empty().append("<h1>" + no + "</h1>");
+  $('.txt-box-2').empty().append("<h4>" + tx + "</h4>");
+  $('.txt-box-3').empty().append("<h3>" + name + "</h3>");
+  $('.txt-box-4').empty().append("<h3>" + site + "</h3>");
+}
+
+function Slider1__init() {
+  $('.slider-1 > .owl-carousel').owlCarousel({
+    dots: false,
+    nav: false,
+    mouseDrag:true,
+    autoplay:false,
+    autoplayTimeout:false,
+    margin:43,
+    
+    responsive: {
+      0: {
+        items: 3
+      }
+    }
+  });
+}
+
+
 $(function(){
   FullPage();
+  slider();
+  Slider1__itemClick(el);
 });
+
+$(function(){
+  Slider1__init();
+})
--- public/portfolio/index.php
+++ public/portfolio/index.php
@@ -13,17 +13,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
     <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
 
     <script src="/portfolio/index.js"></script>
 </head>
 
 <body>
-    <div id="fullpage">
+    <div id="fullpage" class="con-min">
         <div class="section section1">
             <div class="page-1">
                 <div class="copy">
@@ -47,6 +46,17 @@
         </div>
         <div class="section section2">
             <div class="page-2">
+                <div class="tit">
+                    <div class="line-box">
+                        <div class="line-1"></div>
+                        <div class="line-2"></div>
+                        <div class="line-3"></div>
+                        <span class="sy">J.SY</span>
+                        <span class="pf">PORTFOLIO</span>
+                        <span class="ui">Web publisher</span>
+                    </div>
+                </div>
+
                 <div class="infor">
                     <div class="poto">
                     </div>
@@ -55,9 +65,48 @@
                         <div class="sk-box">
                             <ul>
                                 <li class="skill sk-html">
-                                    <div class="txt">html</div>
+                                    <div class="txt">HTML</div>
                                     <div class="progress-bar">
                                     </div>
+                                </li>
+                                <li class="skill sk-html">
+                                    <div class="txt">CSS</div>
+                                    <div class="progress-bar">
+                                    </div>
+                                </li>
+                                <li class="skill sk-html">
+                                    <div class="txt">JS</div>
+                                    <div class="progress-bar">
+                                    </div>
+                                </li>
+                                <li class="skill sk-html">
+                                    <div class="txt">PS</div>
+                                    <div class="progress-bar">
+                                    </div>
+                                </li>
+                                <li class="skill sk-html">
+                                    <div class="txt">AI</div>
+                                    <div class="progress-bar">
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="profile">
+                        <h4 class="pro-tit">PROFILE</h4>
+                        <div class="pro-box">
+                            <ul class="flex">
+                                <li>
+                                    <div>Name</div>
+                                    <div>Birthday</div>
+                                    <div>E-maill</div>
+                                    <div>Address</div>
+                                </li>
+                                <li class="addr">
+                                    <p>정서윤 / Jung Seo Yun</p>
+                                    <p>1990.06.10</p>
+                                    <p>jms2464@naver.com</p>
+                                    <p>대전 동구</p>
                                 </li>
                             </ul>
                         </div>
@@ -65,9 +114,277 @@
                 </div>
             </div>
         </div>
-        <div class="section section3">페이지 4</div>
-        <div class="section section4">페이지 5</div>
-        <div class="section section5">페이지 6</div>
+        <div class="section section3">
+            <div class="page-3">
+                <div class="tit">
+                    <div class="line-box">
+                        <div class="line-1"></div>
+                        <span class="sy">J.SY</span>
+                        <span class="pf">PORTFOLIO</span>
+                        <span class="ui">Web publisher</span>
+                    </div>
+                </div>
+
+                <div class="ux-ui">
+                    <div class="ux-tit">
+                        <h1>UX&UI</h1>
+                        <h1>Design</h1>
+                    </div>
+                    <div class="sub">
+                        <p>The value of an action lies in <br> achieving that action to the end.</p>
+                    </div>
+                </div>
+
+                <div class="Thumbnail">
+                    <div class="img-box-1 box"></div>
+                </div>
+                <div class="tn-box">
+                    <div class="txt-box-1"></div>
+                    <div class="txt-box-2"></div>
+                    <div class="txt-box-3"></div>
+                    <div class="txt-box-4"></div>
+                </div>
+
+                <div class="slider">
+                    <div class="carousel-1 active">
+                        <div class="owl-carousel owl-theme">
+                            <div class="item" data-site="http://www.futurasoft.co.kr/" data-name="원본사이트" data-tx="Copy"
+                                data-no="FUTURASOFT" data-link-url="../pf/fs/index.php"
+                                data-img-url="https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/fs.png?dummy123123"
+                                onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/fsorft/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="MIRUN" data-link-url="https://www.daum.net"
+                                data-img-url="https://placekitten.com/700/350" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/mirun/logo2.png"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="3" data-link-url="https://www.naver.com"
+                                data-img-url="https://placekitten.com/700/350" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/subway/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="4" data-link-url="https://www.naver.com"
+                                data-img-url="https://placekitten.com/408/287" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/subway/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="3" data-link-url="https://www.naver.com"
+                                data-img-url="https://placekitten.com/700/350" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/subway/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="3" data-link-url="https://www.naver.com"
+                                data-img-url="https://placekitten.com/700/350" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/subway/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+
+                            <div class="item" data-site="https://www.subway.co.kr" data-name="원본사이트" data-tx="Copy"
+                                data-no="3" data-link-url="https://www.naver.com"
+                                data-img-url="https://placekitten.com/700/350" onclick="Slider1__itemClick(this);">
+                                <div class="img-box">
+                                    <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/subway/logo.png?dummy123123"
+                                        alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="section section4">
+            <div class="page-4">
+                <div class="tit">
+                    <div class="line-box">
+                        <div class="line-1"></div>
+                        <div class="line-2"></div>
+                        <div class="line-3"></div>
+                        <span class="sy">J.SY</span>
+                        <span class="pf">PORTFOLIO</span>
+                        <span class="ui">Web publisher</span>
+                    </div>
+                </div>
+
+                <div class="ux-ui">
+                    <div class="ux-tit">
+                        <h1>Creative Design</h1>
+                    </div>
+                    <div class="sub">
+                        <p>The value of an action lies in <br> achieving that action to the end.</p>
+                    </div>
+                </div>
+
+                <div class="slider-1 con">
+                    <div class="owl-carousel owl-theme ">
+                        <div class="item">
+                            <a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn5.jpg);">
+                                </div>
+
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn12.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn7.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn4.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn1.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn6.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn2.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn8.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn9.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn10.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn11.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item"><a href="#">
+                                <div class="sd-box"
+                                    style="background-image: url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/port/bn3.jpg);">
+                                </div>
+                                <div class="bannr">
+                                    <h3>
+                                        Banner</h3>
+                                    <p>JO MALONE</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="section section5">
+            <div class="page-5">
+
+            </div>
+        </div>
     </div>
 </body>
 
