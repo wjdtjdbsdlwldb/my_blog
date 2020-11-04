a39dc1167c14199cf6c48d345724efa7ffc26cf1
wjdtjdbsdlwldb <jms2464@naver.com>
2020-11-04 23:07:28 +0900

    펄어비스 더럽게 손많이 가네

--- public/pf/pa/index.js
+++ public/pf/pa/index.js
@@ -26,14 +26,151 @@ function hoverBar__init() {
 
     $(".hover-bar-nav").mouseleave(function () {
         var $this = $(this);
+
         var $hoverBar = $(".hover-bar");
         $hoverBar.css("width", "");
         $hoverBar.css("left", "");
     });
 }
 
+function Slider1__init() {
+    $('.slider-1 > .owl-carousel').owlCarousel({
+        dots: true,
+        nav: true,
+        mouseDrag: false,
+        autoplay: true,
+        loop: true,
+        autoplayTimeout: 6000,
+
+        responsive: {
+            0: {
+                items: 1
+            }
+
+        }
+    });
+}
+
+
+function Text_slide() {
+    $('.container .owl-carousel').owlCarousel({
+        dots: false,
+        nav: false,
+        mouseDrag: false,
+        autoplay: true,
+        loop: false,
+        autoWidth: false,
+        autoplayTimeout: 6000,
+
+        responsive: {
+            0: {
+                items: 4
+            }
+
+        }
+    });
+}
+
+
+// 탑바 스크롤 색상변화
+
+$(document).ready(function () {
+    var $topBar = $(".top-wrap");
+
+    var windowHeight = $(window).height();
+
+    $(window).resize(function () {
+        windowHeight = $(window).height();
+    });
+
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > windowHeight - 700) {
+            $topBar.addClass("white");
+        } else {
+            $topBar.removeClass("white");
+        }
+    });
+});
+
+
+/* 발견되면 활성화시키는 라이브러리 시작 */
+function ActiveOnVisible__init() {
+    $(window).resize(ActiveOnVisible__initOffset);
+    ActiveOnVisible__initOffset();
+
+    $(window).scroll(ActiveOnVisible__checkAndActive);
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    $(".active-on-visible").each(function (index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+        var offsetBottom = offsetTop + $node.outerHeight();
+        $node.attr("data-active-on-visible-offsetTop", offsetTop);
+        $node.attr("data-active-on-visible-offsetBottom", offsetBottom);
+
+        if (!$node.attr("data-active-on-visible-diff-y")) {
+            $node.attr("data-active-on-visible-diff-y", "-100");
+        }
+
+        if (!$node.attr("data-active-on-visible-delay")) {
+            $node.attr("data-active-on-visible-delay", "100");
+        }
+    });
+
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() {
+    $(".active-on-visible").each(function (index, node) {
+        var $node = $(node);
+
+        var offsetTop = parseInt($node.attr("data-active-on-visible-offsetTop"));
+        var offsetBottom = parseInt(
+            $node.attr("data-active-on-visible-offsetBottom")
+        );
+        var diffY = parseInt($node.attr("data-active-on-visible-diff-y"));
+        var delay = parseInt($node.attr("data-active-on-visible-delay"));
+
+        if ($(window).scrollTop() + diffY > offsetBottom == false) {
+            if ($(window).scrollTop() + $(window).height() - diffY > offsetTop) {
+                setTimeout(function () {
+                    $node.addClass("active");
+                }, delay);
+            }
+        }
+    });
+
+    $(".active-on-visible.active").each(function (index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.attr("data-active-on-visible-offsetTop");
+        var offsetBottom = $node.attr("data-active-on-visible-offsetBottom");
+        var diffY = parseInt($node.attr("data-active-on-visible-diff-y"));
+        var delay = parseInt($node.attr("data-active-on-visible-delay"));
+
+        if (
+            $(window).scrollTop() + $(window).height() < offsetTop ||
+            $(window).scrollTop() > offsetBottom
+        ) {
+            setTimeout(function () {
+                $node.removeClass("active");
+            }, delay);
+        }
+    });
+}
+
+
+
 
 $(function () {
     MenuEffect__init();
     hoverBar__init();
+    Slider1__init();
+    Text_slide();
+    ActiveOnVisible__init();
 });
--- public/pf/pa/index.php
+++ public/pf/pa/index.php
@@ -7,97 +7,172 @@
     <title>펄어비스 | Pearl Abyss</title>
     <link rel="stylesheet" href="./index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <script src="./index.js"></script>
 </head>
 
 <body>
-    <section class="main">
-        <div class="header">
-            <div class="top-wrap">
-                <div class="top-bar flex flex-ai-c">
-                    <div class="logo flex-grow-1">
-                        <a href="#" class="block"></a>
+    <div class="page_container">
+        <section class="main">
+            <div class="header">
+                <div class="top-wrap">
+                    <div class="top-bar con flex flex-ai-c">
+                        <div class="logo flex-grow-1">
+                            <a href="#" class="block"></a>
+                        </div>
+                        <nav class="menu-box text-align-center flex-grow-1">
+                            <ul class="flex hover-bar-nav">
+                                <li class="hover-bar-menu"><a href="#">소개</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">펄어비스 소개</a></li>
+                                            <li><a href="#">연구소</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="hover-bar-menu"><a href="#">게임</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">검은사막</a></li>
+                                            <li><a href="#">검은사막 모바일</a></li>
+                                            <li><a href="#">섀도우 아레나</a></li>
+                                            <li><a href="#">붉은사막</a></li>
+                                            <li><a href="#">도깨비</a></li>
+                                            <li><a href="#">플랜 8</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="hover-bar-menu"><a href="#">미디어</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">보도자료</a></li>
+                                            <li><a href="#">미디어 행사</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="hover-bar-menu"><a href="#">문화</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">기업문화</a></li>
+                                            <li><a href="#">복지</a></li>
+                                            <li><a href="#">펄어비스인</a></li>
+                                            <li><a href="#">펄어비스 소식</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="hover-bar-menu"><a href="#">채용</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">채용소개</a></li>
+                                            <li><a href="#">지원하기</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="hover-bar-menu"><a href="#">투자정보</a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="#">기업지배구조</a></li>
+                                            <li><a href="#">주가정보</a></li>
+                                            <li><a href="#">재무정보</a></li>
+                                            <li><a href="#">공시·공고</a></li>
+                                            <li><a href="#">IR 자료</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="hover-bar"></div>
+                        </nav>
+                        <nav class="lang flex">
+                            <ul class="flex">
+                                <li><a href="#">KOR</a></li>
+                                <li><a href="#">ENG</a></li>
+                            </ul>
+                        </nav>
                     </div>
-                    <nav class="menu-box flex-grow-1 text-align-center">
-                        <ul class="flex hover-bar-nav">
-                            <li class="hover-bar-menu"><a href="#">소개</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">펄어비스 소개</a></li>
-                                        <li><a href="#">연구소</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="hover-bar-menu"><a href="#">게임</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">검은사막</a></li>
-                                        <li><a href="#">검은사막 모바일</a></li>
-                                        <li><a href="#">섀도우 아레나</a></li>
-                                        <li><a href="#">붉은사막</a></li>
-                                        <li><a href="#">도깨비</a></li>
-                                        <li><a href="#">플랜 8</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="hover-bar-menu"><a href="#">미디어</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">보도자료</a></li>
-                                        <li><a href="#">미디어 행사</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="hover-bar-menu"><a href="#">문화</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">기업문화</a></li>
-                                        <li><a href="#">복지</a></li>
-                                        <li><a href="#">펄어비스인</a></li>
-                                        <li><a href="#">펄어비스 소식</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="hover-bar-menu"><a href="#">채용</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">채용소개</a></li>
-                                        <li><a href="#">지원하기</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="hover-bar-menu"><a href="#">투자정보</a>
-                                <div class="sub-menu">
-                                    <ul>
-                                        <li><a href="#">기업지배구조</a></li>
-                                        <li><a href="#">주가정보</a></li>
-                                        <li><a href="#">재무정보</a></li>
-                                        <li><a href="#">공시·공고</a></li>
-                                        <li><a href="#">IR 자료</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="hover-bar"></div>
-                    </nav>
-
-                    <nav class="lang-box">
-                        <ul class="flex">
-                            <li><a href="#">KOR</a></li>
-                            <li><a href="#" class="eng">ENG</a></li>
-                        </ul>
-                    </nav>
+                    <div class="sub-box"></div>
                 </div>
             </div>
-        </div>
 
-        <div class="main-slider">
 
-        </div>
-    </section>
+            <div class="slider-1">
+                <div class="owl-carousel owl-theme text-align-center">
+                    <div class="item"
+                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/MMYPV8QO5YRX0F7820200908081836459.jpg?dummy123123)">
+                        <h3 class="bg-title">
+                            새롭고, 흥미롭고, 잊지 못할 모험을 선사하는<br>자기혁신적인 게임 회사, 펄어비스</h3>
+                    </div>
+                    <div class="item"
+                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/XA5O7X5WZ2WHMID220200908081951677.jpg?dummy123123)">
+                        <h3 class="bg-title">
+                            최상의 게임 경험을 전달하기 위해<br>기술을 연구합니다 </h3>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="container">
+            <div class="new-box">
+                <h3 class="n-tit">NEWS</h3>
+                <div class="ns-txt">
+                    <div class="owl-carousel owl-theme">
+                        <a href="#" class="item">
+                            <h4>펄어비스 검은사막 신규·복귀 이용자 위한 '오색빛 월광석’ 이벤트</h4>
+                            <p class="day">2020.11.04</p>
+                            <p>펄어비스(대표 정경인)가 오픈월드 MMORPG ‘검은사막’의 신규 이벤트 ‘오색빛 월광석을 위한 도전’을 오늘(4일)부터 12월 1일까지 진행한다.
+                            </p>
+                        </a>
+                        <a href="#" class="item">
+                            <h4>펄어비스 검은사막 콘솔, 차세대 기기 게임 플레이 지원</h4>
+                            <p class="day">2020.11.03</p>
+                            <p>펄어비스(대표 정경인)가 ‘검은사막 콘솔’의 차세대 기기(Xbox Series X/S, PlayStation 5) 지원 계획을 발표했다.</p>
+                        </a>
+                        <a href="#" class="item">
+                            <h4>펄어비스, 검은사막 모바일 신규 클래스 ‘수라’ 사전생성 시작</h4>
+                            <p class="day">2020.11.03</p>
+                            <p>펄어비스(대표 정경인)가 검은사막 모바일 신규 클래스 ‘수라’의 사전생성 이벤트를 오늘(3일)부터 시작한다. </p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="intro flex">
+                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="0">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_company.jpg?dummy123123"
+                            alt="">
+                    </div>
+                </a>
+                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="100">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_lab.jpg?dummy123123"
+                            alt="">
+                    </div>
+                </a>
+                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="200">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_culture.jpg?dummy123123"
+                            alt="">
+                    </div>
+                </a>
+                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="300">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_in.jpg?dummy123123"
+                            alt="">
+                    </div>
+                </a>
+                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="400">
+                    <div class="img-box">
+                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_news.jpg?dummy123123"
+                            alt="">
+                    </div>
+                </a>
+            </div>
+        </section>
+    </div>
+
 </body>
 
 </html>
