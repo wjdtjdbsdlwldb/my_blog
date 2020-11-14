a7fd2fe69cb175cb15f123cf7c4032afc9ce5ee6
wjdtjdbsdlwldb <jms2464@naver.com>
2020-11-14 14:46:10 +0900

    기본틀완성 탑버튼완성, 반응형만 만들면됨

--- public/pf/pa/index.js
+++ public/pf/pa/index.js
@@ -1,176 +1,234 @@
 function MenuEffect__init() {
-    var $Enter = $(".main > .header");
+  var $Enter = $(".main > .header");
 
-    $Enter.mouseenter(function () {
-        $Enter.addClass("active");
-    });
+  $Enter.mouseenter(function () {
+    $Enter.addClass("active");
+  });
 
-    $Enter.mouseleave(function () {
-        $Enter.removeClass("active");
-    });
+  $Enter.mouseleave(function () {
+    $Enter.removeClass("active");
+  });
 }
 
 function hoverBar__init() {
-    $(".hover-bar-nav .hover-bar-menu").mouseenter(function () {
-        var $this = $(this);
+  $(".hover-bar-nav .hover-bar-menu").mouseenter(function () {
+    var $this = $(this);
 
-        var menuWidth = $this.width();
-        var menuPadding = ($this.innerWidth() - $this.width()) / 2;
-        var menuPosition = $this.position().left;
+    var menuWidth = $this.width();
+    var menuPadding = ($this.innerWidth() - $this.width()) / 2;
+    var menuPosition = $this.position().left;
 
-        var $hoverBar = $(".hover-bar");
+    var $hoverBar = $(".hover-bar");
 
-        $hoverBar.css("width", menuWidth + "px");
-        $hoverBar.css("left", menuPosition + menuPadding + "px");
-    });
+    $hoverBar.css("width", menuWidth + "px");
+    $hoverBar.css("left", menuPosition + menuPadding + "px");
+  });
 
-    $(".hover-bar-nav").mouseleave(function () {
-        var $this = $(this);
+  $(".hover-bar-nav").mouseleave(function () {
+    var $this = $(this);
 
-        var $hoverBar = $(".hover-bar");
-        $hoverBar.css("width", "");
-        $hoverBar.css("left", "");
-    });
+    var $hoverBar = $(".hover-bar");
+    $hoverBar.css("width", "");
+    $hoverBar.css("left", "");
+  });
 }
 
+// 메인 이미지 슬라이드
 function Slider1__init() {
-    $('.slider-1 > .owl-carousel').owlCarousel({
-        dots: true,
-        nav: true,
-        mouseDrag: false,
-        autoplay: true,
-        loop: true,
-        autoplayTimeout: 6000,
-
-        responsive: {
-            0: {
-                items: 1
-            }
-
-        }
-    });
+  $('#owl-one').owlCarousel({
+    dots: true,
+    nav: true,
+    mouseDrag: false,
+    autoplay: true,
+    loop: true,
+    autoplayTimeout: 6000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      200: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: true
+      }
+    }
+  });
 }
 
-
+// 뉴스 슬라이드
 function Text_slide() {
-    $('.container .owl-carousel').owlCarousel({
-        dots: false,
-        nav: false,
-        mouseDrag: false,
-        autoplay: true,
-        loop: false,
-        autoWidth: false,
-        autoplayTimeout: 6000,
+  $('#owl-two').owlCarousel({
+    dots: false,
+    nav: false,
+    mouseDrag: false,
+    autoplay: true,
+    loop: false,
+    autoWidth: false,
+    autoplayTimeout: 6000,
 
-        responsive: {
-            0: {
-                items: 4
-            }
+    responsive: {
+      0: {
+        items: 3
+      }
 
-        }
-    });
+    }
+  });
+}
+
+// 스토리 슬라이드
+function SubNews__init() {
+  $('#owl-three').owlCarousel({
+    dots: false,
+    nav: false,
+    mouseDrag: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+
+    responsive: {
+      0: {
+        items: 4
+      }
+
+    }
+  });
 }
 
 
 // 탑바 스크롤 색상변화
 
 $(document).ready(function () {
-    var $topBar = $(".top-wrap");
+  var $topBar = $(".top-wrap");
 
-    var windowHeight = $(window).height();
+  var windowHeight = $(window).height();
 
-    $(window).resize(function () {
-        windowHeight = $(window).height();
-    });
+  $(window).resize(function () {
+    windowHeight = $(window).height();
+  });
 
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
 
-        if (scrollTop > windowHeight - 700) {
-            $topBar.addClass("white");
-        } else {
-            $topBar.removeClass("white");
-        }
-    });
+    if (scrollTop > windowHeight - 700) {
+      $topBar.addClass("white");
+    } else {
+      $topBar.removeClass("white");
+    }
+  });
 });
 
 
 /* 발견되면 활성화시키는 라이브러리 시작 */
 function ActiveOnVisible__init() {
-    $(window).resize(ActiveOnVisible__initOffset);
-    ActiveOnVisible__initOffset();
+  $(window).resize(_.debounce(ActiveOnVisible__initOffset, 500));
+  ActiveOnVisible__initOffset();
 
-    $(window).scroll(ActiveOnVisible__checkAndActive);
-    ActiveOnVisible__checkAndActive();
+  $(window).scroll(_.debounce(ActiveOnVisible__checkAndActive, 50));
+  ActiveOnVisible__checkAndActive();
 }
 
 function ActiveOnVisible__initOffset() {
-    $(".active-on-visible").each(function (index, node) {
-        var $node = $(node);
+  $('.active-on-visible:not(.actived)').each(function (index, node) {
+    var $node = $(node);
 
-        var offsetTop = $node.offset().top;
-        var offsetBottom = offsetTop + $node.outerHeight();
-        $node.attr("data-active-on-visible-offsetTop", offsetTop);
-        $node.attr("data-active-on-visible-offsetBottom", offsetBottom);
+    var offsetTop = $node.offset().top;
+    $node.attr('data-active-on-visible-offsetTop', offsetTop);
 
-        if (!$node.attr("data-active-on-visible-diff-y")) {
-            $node.attr("data-active-on-visible-diff-y", "-100");
-        }
+    if (!$node.attr('data-active-on-visible-diff-y')) {
+      $node.attr('data-active-on-visible-diff-y', '0');
+    }
 
-        if (!$node.attr("data-active-on-visible-delay")) {
-            $node.attr("data-active-on-visible-delay", "100");
-        }
-    });
+    if (!$node.attr('data-active-on-visible-delay')) {
+      $node.attr('data-active-on-visible-delay', '0');
+    }
 
-    ActiveOnVisible__checkAndActive();
+    var diffY = $node.attr('data-active-on-visible-diff-y');
+    var delay = $node.attr('data-active-on-visible-delay');
+
+    if (diffY.substr(-2, 2) == 'vh') {
+      diffY = parseInt(diffY);
+
+      diffY = diffY * $(window).height() / 100;
+    } else if (diffY.substr(-1, 1) == '%') {
+      diffY = parseInt(diffY);
+
+      diffY = diffY * $node.height() / 100;
+    } else {
+      diffY = parseInt(diffY);
+    }
+
+    $node.data('data-active-on-visible-diff-y', diffY);
+    $node.data('data-active-on-visible-delay', delay);
+  });
+
+  ActiveOnVisible__checkAndActive();
 }
 
 function ActiveOnVisible__checkAndActive() {
-    $(".active-on-visible").each(function (index, node) {
-        var $node = $(node);
+  $('.active-on-visible:not(.actived)').each(function (index, node) {
+    var $node = $(node);
 
-        var offsetTop = parseInt($node.attr("data-active-on-visible-offsetTop"));
-        var offsetBottom = parseInt(
-            $node.attr("data-active-on-visible-offsetBottom")
-        );
-        var diffY = parseInt($node.attr("data-active-on-visible-diff-y"));
-        var delay = parseInt($node.attr("data-active-on-visible-delay"));
+    var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+    var diffY = parseInt($node.data('data-active-on-visible-diff-y'));
+    var delay = parseInt($node.data('data-active-on-visible-delay'));
 
-        if ($(window).scrollTop() + diffY > offsetBottom == false) {
-            if ($(window).scrollTop() + $(window).height() - diffY > offsetTop) {
-                setTimeout(function () {
-                    $node.addClass("active");
-                }, delay);
-            }
+    var callbackFuncName = $node.attr('data-active-on-visible-callback-func-name');
+
+    var scrollTop = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var nodeHeight = $node.height();
+
+    if (scrollTop + windowHeight + diffY > offsetTop) {
+      setTimeout(function () {
+        $node.addClass('active');
+
+        if ($node.hasClass('active-only-one')) {
+          $node.addClass('actived');
         }
-    });
 
-    $(".active-on-visible.active").each(function (index, node) {
-        var $node = $(node);
-
-        var offsetTop = $node.attr("data-active-on-visible-offsetTop");
-        var offsetBottom = $node.attr("data-active-on-visible-offsetBottom");
-        var diffY = parseInt($node.attr("data-active-on-visible-diff-y"));
-        var delay = parseInt($node.attr("data-active-on-visible-delay"));
-
-        if (
-            $(window).scrollTop() + $(window).height() < offsetTop ||
-            $(window).scrollTop() > offsetBottom
-        ) {
-            setTimeout(function () {
-                $node.removeClass("active");
-            }, delay);
+        if (window[callbackFuncName]) {
+          window[callbackFuncName]($node);
         }
-    });
+      }, delay);
+    } else {
+      $node.removeClass('active');
+    }
+  });
 }
 
-
-
+/* 발견되면 활성화시키는 라이브러리 끝 */
 
 $(function () {
-    MenuEffect__init();
-    hoverBar__init();
-    Slider1__init();
-    Text_slide();
-    ActiveOnVisible__init();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('.button').fadeIn();
+    } else {
+      $('.button').fadeOut();
+      $('.button').css('display', 'none');
+    }
+  });
+
+  $(".button").click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+});
+
+$(function () {
+  MenuEffect__init();
+  hoverBar__init();
+  Slider1__init();
+  Text_slide();
+  ActiveOnVisible__init();
+  SubNews__init();
 });
--- public/pf/pa/index.php
+++ public/pf/pa/index.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>펄어비스 | Pearl Abyss</title>
     <link rel="stylesheet" href="./index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/smooth-scrollbar/8.5.3/smooth-scrollbar.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet"
@@ -15,7 +17,7 @@
 </head>
 
 <body>
-    <div class="page_container">
+    <div class="page-main">
         <section class="main">
             <div class="header">
                 <div class="top-wrap">
@@ -25,7 +27,8 @@
                         </div>
                         <nav class="menu-box text-align-center flex-grow-1">
                             <ul class="flex hover-bar-nav">
-                                <li class="hover-bar-menu"><a href="#">소개</a>
+                                <li class="hover-bar-menu">
+                                    <a href="#">소개</a>
                                     <div class="sub-menu">
                                         <ul>
                                             <li><a href="#">펄어비스 소개</a></li>
@@ -98,7 +101,7 @@
 
 
             <div class="slider-1">
-                <div class="owl-carousel owl-theme text-align-center">
+                <div id="owl-one" class="owl-carousel owl-theme text-align-center">
                     <div class="item"
                         style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/MMYPV8QO5YRX0F7820200908081836459.jpg?dummy123123)">
                         <h3 class="bg-title">
@@ -118,7 +121,7 @@
             <div class="new-box">
                 <h3 class="n-tit">NEWS</h3>
                 <div class="ns-txt">
-                    <div class="owl-carousel owl-theme">
+                    <div id="owl-two" class="owl-carousel owl-theme">
                         <a href="#" class="item">
                             <h4>펄어비스 검은사막 신규·복귀 이용자 위한 '오색빛 월광석’ 이벤트</h4>
                             <p class="day">2020.11.04</p>
@@ -138,39 +141,200 @@
                     </div>
                 </div>
             </div>
-            <div class="intro flex">
-                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="0">
-                    <div class="img-box">
+
+            <div class="intro flex active-on-visible" data-active-on-visible-diff-y="-5%">
+                <span class="int-img int-1">
+                    <div class="img-box text-align-center">
                         <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_company.jpg?dummy123123"
                             alt="">
                     </div>
-                </a>
-                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="100">
-                    <div class="img-box">
+                    <div class="infor">
+                        <h3>펄어비스</h3>
+                        <p>우리의 비전과 가치</p>
+                    </div>
+                </span>
+                <span class="int-img int-2">
+                    <div class="img-box text-align-center">
                         <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_lab.jpg?dummy123123"
                             alt="">
                     </div>
-                </a>
-                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="200">
-                    <div class="img-box">
+                    <div class="infor">
+                        <h3>연구소</h3>
+                        <p>한계를 뛰어넘는 곳</p>
+                    </div>
+                </span>
+                <span class="int-img int-3">
+                    <div class="img-box text-align-center">
                         <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_culture.jpg?dummy123123"
                             alt="">
                     </div>
-                </a>
-                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="300">
-                    <div class="img-box">
+                    <div class="infor">
+                        <h3>문화</h3>
+                        <p>파괴적인 창조자</p>
+                    </div>
+                </span>
+                <span class="int-img int-4">
+                    <div class="img-box text-align-center">
                         <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_in.jpg?dummy123123"
                             alt="">
                     </div>
-                </a>
-                <a href="#" class="int-img active-on-visible" data-active-on-visible-delay="400">
-                    <div class="img-box">
+                    <div class="infor">
+                        <h3>펄어비스인</h3>
+                        <p>꿈을 실현하는 사람들</p>
+                    </div>
+                </span>
+                <span class="int-img int-5">
+                    <div class="img-box text-align-center">
                         <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/main_category_news.jpg?dummy123123"
                             alt="">
                     </div>
-                </a>
+                    <div class="infor">
+                        <h3>소식</h3>
+                        <p>생생한 펄어비스 이야기</p>
+                    </div>
+                </span>
+            </div>
+
+            <div class="story active-on-visible" data-active-on-visible-diff-y="-5%">
+                <div class="sb-box">
+                    <nav class="sub-news">
+                        <ul id="owl-three" class="owl-carousel owl-theme ">
+                            <li class="item">
+                                <a href="#" class="str-box str1 block">
+                                    <span class="sub-img block">
+                                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/YI87T1PPOV9HO7Q720201030100757920.400x225.jpg?dummy123123"
+                                            alt="">
+                                    </span>
+                                    <div class="sub-txt">
+                                        <p>펄어비스 소식</p>
+                                        <p>펄어비스 방송 스튜디오 오픈</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="item">
+                                <a href="#" class="str-box str2 block">
+                                    <span class="sub-img block">
+                                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/7X84M5DOWHTJUTM120201013100135639.400x225.jpg?dummy123123"
+                                            alt="">
+                                    </span>
+                                    <div class="sub-txt">
+                                        <p>펄어비스인</p>
+                                        <p>링 위의 경기처럼, 매 순간 최선을 다하는 게임 개발자 이야기</p>
+                                    </div>
+                                </a></li>
+                            <li class="item">
+                                <a href="#" class="str-box str3 block">
+                                    <span class="sub-img block">
+                                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/SAS8EZ822ISHH58320200909065518833.400x225.jpg?dummy123123"
+                                            alt="">
+                                    </span>
+                                    <div class="sub-txt">
+                                        <p>펄어비스 소식</p>
+                                        <p>펄어비스의 사회공헌 활동</p>
+                                    </div>
+                                </a></li>
+                            <li class="item">
+                                <a href="#" class="str-box str4 block">
+                                    <span class="sub-img block">
+                                        <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/O7ANE2O61LKJX4RS20200908133249843.400x225.jpg?dummy123123"
+                                            alt="">
+                                    </span>
+                                    <div class="sub-txt">
+                                        <p>펄어비스 소식</p>
+                                        <p>펄어비스 신규입사자를 위한 웰컴키트</p>
+                                    </div>
+                                </a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </section>
+
+        <footer class="footer">
+            <div class="ft-box">
+                <nav class="ft-menu">
+                    <ul class="flex ft-wrap">
+                        <li><a href="#">소개</a>
+                            <ul>
+                                <li><a href="#" class="block">펄어비스 소개</a></li>
+                                <li><a href="#" class="block">연구소</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">게임</a>
+                            <ul>
+                                <li><a href="#" class="block">검은사막</a></li>
+                                <li><a href="#" class="block">검은사막 모바일</a></li>
+                                <li><a href="#" class="block">섀도우 아레나</a></li>
+                                <li><a href="#" class="block">붉은사막</a></li>
+                                <li><a href="#" class="block">도깨비</a></li>
+                                <li><a href="#" class="block">플랜 8</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">미디어</a>
+                            <ul>
+                                <li><a href="#" class="block">보도자료</a></li>
+                                <li><a href="#" class="block">미디어 행사</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">문화</a>
+                            <ul>
+                                <li><a href="#" class="block">기업문화</a></li>
+                                <li><a href="#" class="block">복지</a></li>
+                                <li><a href="#" class="block">펄어비스인</a></li>
+                                <li><a href="#" class="block">펄어비스 소식</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">채용</a>
+                            <ul>
+                                <li><a href="#" class="block">채용소개</a></li>
+                                <li><a href="#" class="block icon">지원하기</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">투자정보</a>
+                            <ul>
+                                <li><a href="#" class="block">기업지배구조</a></li>
+                                <li><a href="#" class="block">주가정보</a></li>
+                                <li><a href="#" class="block">재무정보</a></li>
+                                <li><a href="#" class="block">공시·공고</a></li>
+                                <li><a href="#" class="block">IR 자료</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="flex">
+                        <li><a href="#" class="block">광고/제휴 문의</a></li>
+                        <li><a href="#" class="block">기업윤리 신고센터</a></li>
+                        <li><a href="#" class="block">개인정보 처리방침</a></li>
+                        <li><a href="#" class="block icon">펄어비스 스토어</a></li>
+                        <li><a href="#" class="block icon">게임 고객센터</a></li>
+                    </ul>
+                </nav>
+                <div class="copy">
+                    <div class="inner flex flex-ai-c flex-jc-c">
+                        <div class="ft-logo flex">
+                            <a href="#" class="block"></a>
+                            <p class="copyright">Copyright © Pearl Abyss Corp. All Rights Reserved</p>
+                        </div>
+                        <nav class="ft-sns flex">
+                            <ul class="sns flex">
+                                <li><a href="#" class="block"
+                                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/icn_footer_facebook_round.png?dummy123123)"></a>
+                                </li>
+                                <li><a href="#" class="block"
+                                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/icn_footer_youtube_round.png?dummy123123)"></a>
+                                </li>
+                                <li><a href="#" class="block"
+                                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/pa/icn_footer_linkedin_round.png?dummy123123)"></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <button class="button">
+            <a href="#" class="top-btn block"></a>
+        </button>
+
     </div>
 
 </body>
