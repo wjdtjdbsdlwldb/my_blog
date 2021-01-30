5b57a13097fa908eea14365ec14b9a2819654d8c
wjdtjdbsdlwldb <jms2464@naver.com>
2021-01-31 01:34:49 +0900

    국립현대미술관 리뉴얼디자인

--- public/pf/hd/index.php
+++ public/pf/hd/index.php
@@ -1,18 +1,188 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>국립현대미술관</title>
     <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="/pf/hd/font/font.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Slick 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
     <script src="./index.js"></script>
 </head>
+
 <body>
-    <main class="main">
-        <header class="header">
-            
+    <main class="main con-min-width">
+        <header class="header con">
+            <div class="top-bar flex">
+                <div class="logo flex">
+                    <a href="#" class="block"
+                        style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/logo.png?dummy1231232)"></a>
+                </div>
+                <nav class="login-box flex flex-ai-end">
+                    <ul class="flex">
+                        <li><a href="#" class="block">로그인</a></li>
+                        <li><a href="#" class="block">회원가입</a></li>
+                        <li><a href="#" class="block">새소식</a></li>
+                        <li><a href="#" class="block">멤버십</a></li>
+                        <li><a href="#" class="block">사이트맵</a></li>
+                        <li><a href="#" class="block">LANGUAGES</a></li>
+                        <li><a href="#" class="block"><img
+                                    src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/qq.png?dummy1231232"
+                                    alt="돋보기"></a></li>
+                    </ul>
+                </nav>
+            </div>
+            <nav class="menu-box">
+                <ul class="flex flex-jc-c">
+                    <li><a href="#" class="block">관람안내</a></li>
+                    <li><a href="#" class="block">전시</a></li>
+                    <li><a href="#" class="block">교육</a></li>
+                    <li><a href="#" class="block">소장품</a></li>
+                    <li><a href="#" class="block">연구와 출판</a></li>
+                    <li><a href="#" class="block">온라인 미술관</a></li>
+            </nav>
         </header>
+        <div class="sd-box">
+            <div class="main-slide con">
+                <div class="slider1">
+                    <div class="bn1 bn"></div>
+                    <div class="bn2 bn"></div>
+                    <div class="bn3 bn"></div>
+                </div>
+                <div class="circle">
+                    <p>공지사항</p>
+                </div>
+            </div>
+            <div class="under"></div>
+        </div>
+        <div class="noti con">
+            <div class="nt-icon"><img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/sp.png?dummy1231232"
+                    alt="공지사항"></div>
+            <div class="noti-slider">
+                <div class="nt1 nt">
+                    [새소식] 제8기 고객자문단 모집 공고</div>
+                <div class="nt2 nt">
+                    [새소식] 2021년 열린강좌 설문조사</div>
+                <div class="nt3 nt">
+                    [새소식] 국립현대미술관 청주 개방 수장고 일부 변경 운영 안내</div>
+            </div>
+        </div>
+        <div class="online flex">
+            <div class="on-wrap con flex">
+                <div class="txt-box">
+                    <h2>온라인<br>미술관</h2>
+                    <p>국립현대미술관을 온라인으로<br>만나보세요</p>
+                </div>
+                <nav class="on-video flex">
+                    <ul class="flex">
+                        <li>
+                            <a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/online1_1.png?dummy1231232)">
+                                <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/vdicon.png?dummy1231232"
+                                    alt="플레이아이콘">
+                            </a>
+                            <a href="#" class="vd-infor block">
+                                <p class="title">오윤, 춘무인추무의, 1985｜국립현대미술관
+                                    윤범모 관장｜열린강좌 미술관소장품강좌</p>
+                                <p>성인</p>
+                                <p class="date">2020.11.25</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/online2_1.png?dummy1231232)">
+                                <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/vdicon.png?dummy1231232"
+                                    alt="플레이아이콘">
+                            </a>
+                            <a href="#" class="vd-infor block">
+                                <p class="title">박래현, 노점, 1956｜국립현대미술관 윤범모 관장｜열린강좌 미술관소장품강좌</p>
+                                <p>성인</p>
+                                <p class="date">2020.10.28</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/online3_1.png?dummy1231232)">
+                                <img src="https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/vdicon.png?dummy1231232"
+                                    alt="플레이아이콘">
+                            </a>
+                            <a href="#" class="vd-infor block">
+                                <p class="title">채용신, 고종황제어진｜국립현대미술관 윤범모 관장｜열린강좌 미술관소장품강좌</p>
+                                <p>성인</p>
+                                <p class="date">2020.06.15</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="on-more">
+                <P>온라인 미술관<br>더 자세히 알아보기&emsp;></P>
+            </div>
+        </div>
+        <div class="collection"
+            style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/bgmmca.png?dummy123123)">
+            <div class="cl-wrap con">
+                <div class="coll-tit flex flex-ai-c">
+                    <div class="cl-bar"></div>
+                    <h3 class="tit flex-grow-1">소장품</h3>
+                    <div class="cl-more">More</div>
+                </div>
+                <nav class="coll-box">
+                    <ul class="flex">
+                        <li><a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/coll2.png?dummy123123)"></a>
+                            <a href="#" class="cl-infor block">
+                                <p>이종우</p>
+                                <p>LEE Chongwoo</p>
+                                <p>누드-남자</p>
+                                <p class="cl-deta">1926</p>
+                            </a>
+                        </li>
+                        <li><a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/coll3.png?dummy123123)"></a>
+                            <a href="#" class="cl-infor block">
+                                <p>이화자</p>
+                                <p>LEE Whaja</p>
+                                <p>봄 I</p>
+                                <p class="cl-deta">1980</p>
+                            </a>
+                        </li>
+                        <li><a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/gohi.png?dummy123123)"></a>
+                            <a href="#" class="cl-infor block">
+                                <p>고희동</p>
+                                <p>Ko Heitong</p>
+                                <p>자화상</p>
+                                <p class="cl-deta">1915</p>
+                            </a>
+                        </li>
+                        <li><a href="#" class="block"
+                                style="background-image:url(https://wjdtjdbsdlwldb.github.io/img1/blog/article/hd/coll.png?dummy123123)"></a>
+                            <a href="#" class="cl-infor block">
+                                <p>이종우</p>
+                                <p>LEE Chongwoo</p>
+                                <p>루앙풍경</p>
+                                <p class="cl-deta">1926</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="exh-box">
+            <div class="exh-wrap">
+                
+            </div>
+        </div>
     </main>
 </body>
+
 </html>
